69f5934acde010d14e930c4d432d8eef512f24ba
anjibabukk6 <anjibabukk6@gmail.com>
2022-09-15 12:49:51 +0530

    initial commit

---
+++ 1 file.py
@@ -0,0 +1,57 @@
+# # # 1
+# # # 2
+# # # 3
+# # a=2
+# # list_a=[5,"anji",5.6,8,a]
+# # list_b=["babu",45]
+# # list_c=    list_a+list_b
+# # print(list_c[5])
+#
+# #
+# # list_a=[]
+# # print(list_a)
+# # for i in range(1,4):
+# #     list_a=list_a+[i]
+# #     print(list_a)
+# # print(list_a)
+#
+#
+# # list_a=[1,"anji",8,47.3]
+# # # list_b= list_a[:2]
+# # # print(list_b)
+#
+# # list_a=["a","e","q","p","v"]
+# # list_b=list_a[0:5:3]
+# # print(list_b)
+# #
+# # color="red"
+# # list_a=list(color)
+# # print(list_a)
+#
+# # list_a=list(range(6))
+# # print(list_a)
+#
+# # list_a=[1,2,3,5]
+# # # print(list_a[3])
+# # list_a[3]=10
+# # print(list_a)
+#
+# # message="sea you soon"
+# # message[2]="e"
+# # print(message)
+#
+# a="hello"
+# a=a+"world"
+# print(id(a))
+#
+# list_a=[1,2,3,5]
+# list_b=list_a
+# list_b[3]=4
+# print("list a: "+str(list_a))
+# print("list b: "+str(list_b))
+
+list_a=[1,2,3]
+list_b=list_a
+list_a=list_a+[6,7]
+print("list a: "+str(list_a))
+print("list b: "+str(list_b))
